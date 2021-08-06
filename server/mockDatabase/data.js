29f5036b71af11a1a0ce56b4104233e1e795b764
jekb2019 <jekb2019@gmail.com>
2021-08-06 12:50:23 +1200

    Add identity and habit mutation and move content in data.js to schema.js
    Issue: Add add/delete identity and add/delete habit mutation schema. Changing data content of the exported mock data caused error due to read-only nature of exported variables.
    Solution: Add corresponding mutation schema and move the mock data into schema.js and delete contents inside the data.js.
    RisK: Further UI testing may be needed for the newly created mutation schema.
    Reviewd by: Jason Ko

--- server/mockDatabase/data.js
+++ server/mockDatabase/data.js
@@ -1,13 +1,1 @@
-export let identities = [
-    {id: '1', name: 'Best developer', description: 'The best programmer in the world'},
-    {id: '2', name: 'Healthy person', description: 'A person who is fit and healthy'},
-    {id: '3', name: 'Kind person', description: 'A person who respects others'},
-    {id: '4', name: 'Handsome man'},
-]
-
-export let habits = [
-    {id: '1', name:'Study coding', description: 'Watch coding tutorials', identityId: '1'},
-    {id: '2', name:'Do side projects', identityId: '1'},
-    {id: '3', name:'Eat healthy', description: 'No junk food!', identityId: '2'},
-    {id: '4', name:'Be nice', description: 'Smile always', identityId: '3'},
-]
+// The mock data inside data.js is moved to schema.js to enable mutation.
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,27 @@
-import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
-import { identities, habits } from "../mockDatabase/data.js";
+import { UserInputError } from "apollo-server-errors";
+import { GraphQLID, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import { v4 as uuid } from 'uuid';
+
+
+let identities = [
+    {id: '1', name: 'Best developer', description: 'The best programmer in the world'},
+    {id: '2', name: 'Healthy person', description: 'A person who is fit and healthy'},
+    {id: '3', name: 'Kind person', description: 'A person who respects others'},
+    {id: '4', name: 'Handsome man'},
+]
+
+let habits = [
+    {id: '1', name:'Study coding', description: 'Watch coding tutorials', identityId: '1'},
+    {id: '2', name:'Do side projects', identityId: '1'},
+    {id: '3', name:'Eat healthy', description: 'No junk food!', identityId: '2'},
+    {id: '4', name:'Be nice', description: 'Smile always', identityId: '3'},
+]
 
 const IdentityType = new GraphQLObjectType({
     name: 'Identity',
     fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
         description: { type: GraphQLString },
         habits: {
             type: new GraphQLList(HabitType),
@@ -17,12 +33,12 @@ const IdentityType = new GraphQLObjectType({
 const HabitType = new GraphQLObjectType({
     name: 'Habit',
     fields: () => ({
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: new GraphQLNonNull(GraphQLString) },
         description: { type: GraphQLString},
-        identityId: { type: GraphQLID },
+        identityId: { type: new GraphQLNonNull(GraphQLID) },
         identity: {
-            type: IdentityType,
+            type: new GraphQLNonNull(IdentityType),
             resolve: (parent, args) => (identities.find(identity => parent.identityId === identity.id))
         }
     }),
@@ -33,7 +49,6 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         identities: {
             type: new GraphQLList(IdentityType),
-            // TODO (Jason): Try to change resolve to arrow function
             resolve: (parent, args) => (identities),
         },
         identity: {
@@ -53,6 +68,78 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        // Add identity
+        addIdentity: {
+            type: IdentityType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                description: {type: GraphQLString}
+            },
+            resolve: (parent, args) => {
+                const { name, description } = args;
+                if(!name) {
+                    return;
+                }
+                let newIdentity = { id: uuid() , name, description };
+                identities = [newIdentity, ... identities];
+                return newIdentity;
+            },
+        },
+        // Delete identity
+        deleteIdentity: {
+            type: new GraphQLList(IdentityType),
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLID)},
+            },
+            resolve: (parent, args) => {
+                // delete identity
+                identities = identities.filter(identity => args.id !== identity.id)
+                // delete related habits
+                habits = habits.filter(habit => args.id !== habit.identityId);
+                return identities;
+            },
+        },
+        // Add habit
+        addHabit: {
+            type: HabitType,
+            args: {
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                description: {type: GraphQLString},
+                identityId: {type: new GraphQLNonNull(GraphQLID)},
+            },
+            resolve: (parent, args) => {
+                const { name, description, identityId } = args;
+                if(!name || !identityId) {
+                    return;
+                }
+                const relatedIdentity = identities.find(identity => args.identityId === identity.id);
+                console.log(relatedIdentity);
+                if (!relatedIdentity) {
+                    throw new UserInputError("Invalid Identity Id");
+                }
+                let newHabit = { id: uuid() , name, description, identityId };
+                habits = [newHabit, ... habits];
+                return newHabit;
+            },
+        },
+        // Delete habit
+        deleteHabit: {
+            type: new GraphQLList(HabitType),
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLID)},
+            },
+            resolve: (parent, args) => {
+                habits = habits.filter(habit => args.id !== habit.id)
+                return habits;
+            },
+        },
+    }
+})
+
 export default new GraphQLSchema({
     query: RootQuery,
+    mutation: Mutation,
 })
