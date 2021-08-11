105f507b471558799e4ee38e485299447dca9500
Jason Ko <jekb2019@gmail.com>
2021-08-12 00:28:35 +1200

    Add mock data to test card outputs
    Issue: Card outputs needs to be tested
    Solution: Add mock data to test card outputs
    Risk: No risk since no actural network communication happens
    Reviewed by: Jason Ko

--- frontend/src/components/habitsPage/habitsPage.jsx
+++ frontend/src/components/habitsPage/habitsPage.jsx
@@ -3,20 +3,16 @@ import Card from "../common/card/card";
 import Footer from "../common/footer/footer";
 import Header from "../common/header/header";
 import styles from "./habitsPage.module.css";
+// TODO(Jason): Remove Mock Data
+import { habits } from "../../mock/mockData";
 
 const HabitsPage = (props) => (
   <article className={styles.container}>
     <Header currentPage="habits" />
     <section className={styles.contentBox}>
-      <Card title="Something to describe my habit" />
-      <Card title="Something to describe my habit" />
-      <Card title="Something to describe my habit" />
-      <Card title="Something to describe" />
-      <Card title="Something to describe" />
-      <Card title="Something to describe" />
-      <Card title="Something to describe" />
-      <Card title="Something to describe" />
-      <Card title="Something to describe" />
+      {habits.map((habit) => (
+        <Card title={habit.name} />
+      ))}
     </section>
     <Footer description="You are whoever you wish to be" />
   </article>
--- frontend/src/components/identitiesPage/identitiesPage.jsx
+++ frontend/src/components/identitiesPage/identitiesPage.jsx
@@ -3,15 +3,16 @@ import styles from "./identitiesPage.module.css";
 import Footer from "../common/footer/footer";
 import Header from "../common/header/header";
 import Card from "../common/card/card";
+// TODO(Jason): Remove Mock Data
+import { identities } from "../../mock/mockData";
 
 const IdentitiesPage = (props) => (
   <article className={styles.container}>
     <Header currentPage="identities" />
     <section className={styles.contentBox}>
-      <Card title="Something to describe my identity" />
-      <Card title="Something to describe my identity" />
-      <Card title="Something to describe my identity" />
-      <Card title="Something to describe my identity" />
+      {identities.map((identity) => (
+        <Card title={identity.name} />
+      ))}
     </section>
     <Footer description="You are whoever you wish to be" />
   </article>
