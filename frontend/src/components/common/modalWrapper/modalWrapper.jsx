53f237f4521a7e70457ba4cedff22bf02309282c
Jason Ko <jekb2019@gmail.com>
2021-08-12 14:36:07 +1200

    Add NewIdentityModal component
    Issue: Add NewIdentityModal component
    Solution: Add NewIdentityModal component
    Risk: No risk
    Reviewed by: Jason Ko

--- frontend/src/components/common/modalWrapper/modalWrapper.jsx
+++ frontend/src/components/common/modalWrapper/modalWrapper.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NewHabitModal from "../../newHabitModal/newHabitModal";
+import NewIdentityModal from "../../newIdentityModal/newIdentityModal";
 import styles from "./modalWrapper.module.css";
 
 const ModalWrapper = (props) => (
   <div className={styles.container}>
-    <NewHabitModal />
+    {/* <NewHabitModal /> */}
+    <NewIdentityModal />
   </div>
 );
 
---
+++ frontend/src/components/newIdentityModal/newIdentityModal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./newIdentityModal.module.css";
+
+const NewIdentityModal = (props) => (
+  <article className={styles.container}>
+    <button className={styles.closeButton}>X</button>
+    <div className={styles.contentWrapper}>
+      <h2 className={styles.title}>Add New Identity</h2>
+      <form className={styles.form} id="addHabitForm">
+        <div className={styles.field}>
+          <label for="name" className={styles.label}>
+            Name
+          </label>
+          <input className={styles.input} type="text" name="name"></input>
+        </div>
+        <div className={styles.field}>
+          <label for="description" className={styles.label}>
+            Description
+          </label>
+          <textarea
+            className={`${styles.input} ${styles.inputTextArea}`}
+            name="description"
+          ></textarea>
+        </div>
+      </form>
+      <button className={styles.addButton} form="addHabitForm">
+        Add
+      </button>
+    </div>
+  </article>
+);
+
+export default NewIdentityModal;
