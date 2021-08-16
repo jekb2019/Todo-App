bdacf6173bbeede8f9b6718de2d33849fb370a9f
Jason Ko <jekb2019@gmail.com>
2021-08-17 00:55:22 +1200

    Refactor form in add new modals into AddNewForm component
    Issue: Duplicated codes in the forms
    Solution: Refactor into a new component AddNewForm
    Risk: No risk
    Reviewed by: Jason Ko

--- frontend/src/components/newHabitModal/newHabitModal.jsx
+++ frontend/src/components/newHabitModal/newHabitModal.jsx
@@ -1,36 +1,8 @@
 import React from "react";
 import styles from "./newHabitModal.module.css";
-import { validateHabit } from "../../validators/validators";
+import AddNewForm from "../common/addNewForm/addNewForm";
 
 const NewHabitModal = ({ closeModal, addNewHabit }) => {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const formElements = e.currentTarget.elements;
-    const name = formElements.habitName.value;
-    const description = formElements.habitDescription.value;
-    const identity = formElements.habitIdentity.value;
-    const habit = {
-      name,
-      description,
-      identity,
-    };
-
-    try {
-      validateHabit(habit);
-    } catch (e) {
-      alert(e);
-      closeModal();
-      return;
-    }
-    // Add new habit to server
-    addNewHabit(habit).catch((e) => {
-      alert(e);
-      closeModal();
-      return;
-    });
-    closeModal();
-  };
-
   return (
     <article className={styles.container}>
       <button onClick={closeModal} className={styles.closeButton}>
@@ -38,44 +10,12 @@ const NewHabitModal = ({ closeModal, addNewHabit }) => {
       </button>
       <div className={styles.contentWrapper}>
         <h2 className={styles.title}>Add New Habit</h2>
-        <form onSubmit={onSubmit} className={styles.form} id="addHabitForm">
-          <div className={styles.field}>
-            <label htmlFor="name" className={styles.label}>
-              Name
-            </label>
-            <input
-              id="habitName"
-              className={styles.input}
-              type="text"
-              name="name"
-            ></input>
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="description" className={styles.label}>
-              Description
-            </label>
-            <textarea
-              id="habitDescription"
-              className={`${styles.input} ${styles.inputTextArea}`}
-              name="description"
-            ></textarea>
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="identities" className={styles.label}>
-              Identity
-            </label>
-            <select
-              id="habitIdentity"
-              className={styles.input}
-              name="identities"
-            >
-              <option value="i1">Identity 1</option>
-              <option value="i2">Identity 2</option>
-              <option value="i3">Identity 3</option>
-              <option value="i4">Identity 4</option>
-            </select>
-          </div>
-        </form>
+        <AddNewForm
+          type="habit"
+          submitHandler={addNewHabit}
+          // TODO(Jason): use Context for closeModal prop
+          closeModal={closeModal}
+        />
         <button className={styles.addButton} form="addHabitForm">
           Add
         </button>
--- frontend/src/components/newIdentityModal/newIdentityModal.jsx
+++ frontend/src/components/newIdentityModal/newIdentityModal.jsx
@@ -1,34 +1,8 @@
 import React from "react";
 import styles from "./newIdentityModal.module.css";
-import { validateIdentity } from "../../validators/validators";
+import AddNewForm from "../common/addNewForm/addNewForm";
 
 const NewIdentityModal = ({ closeModal, addNewIdentity }) => {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const formElements = e.currentTarget.elements;
-    const name = formElements.identityName.value;
-    const description = formElements.identityDescription.value;
-    const identity = {
-      name,
-      description,
-    };
-
-    try {
-      validateIdentity(identity);
-    } catch (e) {
-      alert(e);
-      closeModal();
-      return;
-    }
-    // Add new identity to server
-    addNewIdentity(identity).catch((e) => {
-      alert(e);
-      closeModal();
-      return;
-    });
-    closeModal();
-  };
-
   return (
     <article className={styles.container}>
       <button onClick={closeModal} className={styles.closeButton}>
@@ -36,30 +10,15 @@ const NewIdentityModal = ({ closeModal, addNewIdentity }) => {
       </button>
       <div className={styles.contentWrapper}>
         <h2 className={styles.title}>Add New Identity</h2>
-        <form onSubmit={onSubmit} className={styles.form} id="addHabitForm">
-          <div className={styles.field}>
-            <label htmlFor="name" className={styles.label}>
-              Name
-            </label>
-            <input
-              id="identityName"
-              className={styles.input}
-              type="text"
-              name="name"
-            ></input>
-          </div>
-          <div className={styles.field}>
-            <label htmlFor="description" className={styles.label}>
-              Description
-            </label>
-            <textarea
-              id="identityDescription"
-              className={`${styles.input} ${styles.inputTextArea}`}
-              name="description"
-            ></textarea>
-          </div>
-        </form>
-        <button className={styles.addButton} form="addHabitForm">
+        {/*  */}
+        <AddNewForm
+          type="identity"
+          submitHandler={addNewIdentity}
+          // TODO(Jason): use Context for closeModal prop
+          closeModal={closeModal}
+        />
+        {/*  */}
+        <button className={styles.addButton} form="addIdentityForm">
           Add
         </button>
       </div>
